16fc93e4cec4a1b8fb5b708d561a5eb8399e8544
Theo Brigitte <theo.brigitte@gmail.com>
2020-11-05 13:43:39 +0100

    hack: variables check + logging

--- hack/check.sh
+++ hack/check.sh
@@ -2,8 +2,17 @@
 
 set -eu
 
-a=$SMS_USER
-a=$SMS_PASS
+## Required env variables
+_=$HARDWARE
+_=COUNTRY
+_=SMS_USER
+_=SMS_PASS
+
 echo "> checking $HARDWARE in $COUNTRY"
 curl -Ss "https://eu.api.ovh.com/1.0/dedicated/server/availabilities?country=$COUNTRY&hardware=$HARDWARE" | jq -e '.[].datacenters[] | select(.availability != "unavailable")'
+
+echo "> $HARDWARE available in $COUNTRY"
+
+echo "> sending message"
 curl -iXPOST https://smsapi.free-mobile.fr/sendmsg -d'{"msg":"'"$HARDWARE is available"'","user":"'"$SMS_USER"'","pass":"'"$SMS_PASS"'"}' -H'Content-Type: application/json'
+echo "> message sent"
