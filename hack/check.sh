46e2715d4d664ddf1c946a821eae736d786a0d0f
Theo Brigitte <theo.brigitte@gmail.com>
2024-07-28 11:19:50 +0200

    change wording in check script

--- hack/check.sh
+++ hack/check.sh
@@ -3,8 +3,8 @@
 # Check Kimsufi server availability
 #
 # Usages:
-# 	DATACENTERS=fr PLAN_CODE=22sk010 OPSGENIE_API_KEY=******** check.sh
-# 	DATACENTERS=fr,gra,rbx,sbg PLAN_CODE=22sk010 NO_SMS=true check.sh
+# 	PLAN_CODE=22sk010 DATACENTERS=fr OPSGENIE_API_KEY=******** check.sh
+# 	PLAN_CODE=22sk010 DATACENTERS=fr,gra,rbx,sbg NO_NOTIFICATION=true check.sh
 
 set -eu
 
@@ -38,16 +38,17 @@ fi
 AVAILABLE_DATACENTERS="$(echo "$DATA" | jq -r '[.[].datacenters[] | select(.availability != "unavailable") | .datacenter] | join(",")')"
 echo_stderr "> checked  $PLAN_CODE available    in $AVAILABLE_DATACENTERS"
 
-# stop when NO_SMS variable is set
-test ! -v NO_SMS
+# stop when NO_NOTIFICATION variable is set
+test ! -v NO_NOTIFICATION
 
 _=$OPSGENIE_API_KEY
 
 # send notification
 message="$PLAN_CODE is available\nhttps://eco.ovhcloud.com/fr/?display=list&range=kimsufi ."
-echo_stderr "> sending message"
+echo_stderr "> sending notification"
 curl -X POST "$OPSGENIE_API_URL" \
     -H "Content-Type: application/json" \
     -H "Authorization: GenieKey $OPSGENIE_API_KEY" \
     -d'{"message": "'"$message"'"}'
-echo_stderr "> message sent"
+echo_stderr
+echo_stderr "> notification sent"
