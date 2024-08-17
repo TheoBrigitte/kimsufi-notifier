d9828b0a15e6d63d3f8d71fbe942aa8cb065077a
Theo Brigitte <theo.brigitte@gmail.com>
2024-08-17 06:04:54 +0200

    add comments and reformat code

--- bin/check.sh
+++ bin/check.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-# Check Kimsufi server availability
+# Check OVH Eco (including Kimsufi) server availability
 #
 # Allowed datacenters:
 #   bhs, ca, de, fr, fra, gb, gra, lon, pl, rbx, sbg, waw
 #
-# Usages:
+# Usage:
 # 	PLAN_CODE=22sk010 DATACENTERS=fr,gra,rbx,sbg check.sh
 # 	PLAN_CODE=22sk010 DATACENTERS=fr,gra,rbx,sbg OPSGENIE_API_KEY=******** check.sh
 
@@ -14,34 +14,36 @@ set -eu
 OPSGENIE_API_URL="https://api.opsgenie.com/v2/alerts"
 OVH_URL="https://eu.api.ovh.com/v1/dedicated/server/datacenter/availabilities?planCode=${PLAN_CODE}&datacenters=${DATACENTERS}"
 
-# Helper function - prints a message to stderr
 echo_stderr() {
     >&2 echo "$@"
 }
 
-# check availability from api
+# Fetch availability from api
 echo_stderr "> checking $PLAN_CODE availability in $DATACENTERS"
 DATA="$(curl -Ss "${OVH_URL}")"
-#DATA="$(echo bob)"
+
+# Check for error: empty data, invalid json, or empty list
 if test -z "$DATA" || ! echo "$DATA" | jq -e . &>/dev/null || echo "$DATA" | jq -e '. | length == 0' &>/dev/null; then
   echo "> failed to fetch data from $OVH_URL"
   exit 1
 fi
 
+# Check for datacenters availability
 if ! echo "$DATA" | jq -e '.[].datacenters[] | select(.availability != "unavailable")' &>/dev/null; then
   echo_stderr "> checked  $PLAN_CODE unavailable  in $DATACENTERS"
   exit 1
 fi
 
+# Print availability
 AVAILABLE_DATACENTERS="$(echo "$DATA" | jq -r '[.[].datacenters[] | select(.availability != "unavailable") | .datacenter] | join(",")')"
 echo_stderr "> checked  $PLAN_CODE available    in $AVAILABLE_DATACENTERS"
 
-# stop when OPSGENIE_API_KEY variable is not set
+# Exit here when OPSGENIE_API_KEY variable is not set
 if [ -z ${OPSGENIE_API_KEY+x} ]; then
   exit 0
 fi
 
-# send notification
+# Send notification via OpsGenie
 message="$PLAN_CODE is available\nhttps://eco.ovhcloud.com/fr/?display=list&range=kimsufi ."
 echo_stderr "> sending notification"
 curl -X POST "$OPSGENIE_API_URL" \
--- bin/list.sh
+++ bin/list.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 #
-# Display available servers from OVH eco catalog
+# Display available servers from OVH Eco (including Kimsufi) catalog
 #
 # Allowed country codes:
 #   CZ, DE, ES, FI, FR, GB, IE, IT, LT, MA, NL, PL, PT, SN, TN
 #
-# Usages:
+# Usage:
 #   COUNTRY=FR names.sh
 
 set -eu
 
-# Helper function - prints a message to stderr
+OVH_URL="https://eu.api.ovh.com/v1/order/catalog/public/eco?ovhSubsidiary=${COUNTRY}"
+
 echo_stderr() {
     >&2 echo "$@"
 }
 
-## required environement variables
-_=$COUNTRY
-
-OVH_URL="https://eu.api.ovh.com/v1/order/catalog/public/eco?ovhSubsidiary=${COUNTRY}"
-
+# Fetch servers from OVH API
 echo_stderr "> fetching servers in $COUNTRY"
 DATA=$(curl -qSs "${OVH_URL}")
+
+# Check for error: empty data, invalid json, or empty list
 if test -z "$DATA" || ! echo "$DATA" | jq -e . &>/dev/null || echo "$DATA" | jq -e '.plans | length == 0' &>/dev/null; then
   echo "> failed to fetch data from $OVH_URL"
   exit 1
 fi
 echo_stderr "> fetched  servers"
 
+# Get currency code
 CURRENCY="$(echo "$DATA" | jq -r '.locale.currencyCode')"
+
+# Print servers
 echo "$DATA" | \
-  jq -r '.plans[] | [.planCode,.blobs.commercial.range,.invoiceName,(.pricings[]|select(.phase == 1)|select(.mode == "default")|.price/100000000)] | @tsv' | \
+  jq -r '.plans[] | [ .planCode, .blobs.commercial.range, .invoiceName, (.pricings[] | select(.phase == 1) | select(.mode == "default") | .price/100000000) ] | @tsv' | \
   sort -k2,2 -k4n,4 -k3h,3 -b | \
   column -t -C "name=PlanCode" -C "name=Category" -C "name=Name" -C "name=Price ($CURRENCY)" -o '    '
