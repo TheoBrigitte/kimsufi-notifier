a8354791bda3c8d772e8f155aa67b1120c1926ec
Theo Brigitte <theo.brigitte@gmail.com>
2024-11-14 22:58:56 +0100

    more comments less dead code

--- front/src/app/components/server.tsx
+++ front/src/app/components/server.tsx
@@ -10,12 +10,15 @@ interface Props {
     data: Server[];
 }
 
+// ServerLines displays the servers table lines
 function ServerLines({category, servers} : {category: string, servers: Server[]}) {
+  // Define the color and icon the status column
   const statusColor = new Map<string, Status>([
       ["available", {color:"text-lime-600", icon:faCheckCircle}],
       ["unavailable", {color:"text-rose-600", icon:faTimesCircle}],
   ]);
 
+  // Highlight changed rows
   const [rowsData, setRowsData] = useState(new Map<string, Server>());
   const [changedRows, setChangedRows] = useState(new Set());
   useEffect(() => {
@@ -39,7 +42,7 @@ function ServerLines({category, servers} : {category: string, servers: Server[]}
     setRowsData(newRowsData);
   }, [servers]); // Re-run effect when servers data changes
 
-    // Clear highlight after a delay
+  // Clear highlight after a delay
   useEffect(() => {
     if (changedRows.size > 0) {
       const timer = setTimeout(() => {
@@ -66,6 +69,7 @@ function ServerLines({category, servers} : {category: string, servers: Server[]}
   )
 }
 
+// ServerCategories displays the servers table lines with a category line before
 function ServerCategories({ordered} : {ordered: {[key: string]: Server[]}}) {
   return (
     Object.entries(ordered).map(([category, servers]) => (
@@ -82,9 +86,11 @@ const ServersTable = ({data} : Props) => {
     return
   }
 
+  // Group servers by category
   const serversByCategory = Object.groupBy(data, ( server: Server ) => server.category);
-
+  // Define the order of categories
   const categoryOrder: {[key: string] :Server[]} = { "Kimsufi": [], "So you Start": [], "Rise": [], "": [], }
+  // Merge the server and respect the categories order
   const ordered = Object.assign(categoryOrder, serversByCategory);
 
   return (
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -15,29 +15,36 @@ type StatusProps = {
   lastMessage: number;
 };
 
+// Status components displays the status of the websocket connection
 function Status({error, data, connectionRestored, setConnectionRestored, lastMessage}: StatusProps) {
   let message;
   let fadeOut;
 
   if (error) {
+    // Display error message
     message =
       <>
         <div>Failed to load server list</div>
         <div className="text-orange-700">{error.toString()}</div>
       </>
   } else if (!data||data==undefined) {
+    // No data yet, still loading
     message = <div>Loading ...</div>
   } else if (connectionRestored) {
+    // Connection restored message
     message = <div className="text-green-700">Websocket connected</div>
     fadeOut = "transition-opacity duration-[2000ms] opacity-0"
+    // Reset connectionRestored after 5 seconds
     setTimeout(function() {
       setConnectionRestored(false);
     }, 5000);
   } else if (lastMessage > 0) {
+    // Last message received timestamp
     const date = new Date(lastMessage).toTimeString().split(' ')[0]
     message = <div>Last update received at {date}</div>
   }
 
+  // Hide the message if there is no message to display
   const hidden = !message ? "hidden" : ""
 
   return (
@@ -48,14 +55,22 @@ function Status({error, data, connectionRestored, setConnectionRestored, lastMes
 }
 
 export default function Home() {
+  // Get the websocket URL from the environment variable
   const websocketURL = process.env.NEXT_PUBLIC_WEBSOCKET_URL || 'ws://localhost';
 
+  // Initialize the state variables
+  //
+  // data is the list of servers received from the websocket server
   const emptyData = Array<Server>();
   const [data, setData] = useState(emptyData);
+  // connectionRestored is used to display a message when the websocket connection is restored
   const [connectionRestored, setConnectionRestored] = useState(false);
+  // lastMessage is the timestamp of the last message received from the websocket server
   const [lastMessage, setLastMessage] = useState(0);
+  // error is the error message from the websocket connection
   const [error, setError] = useState(null as ErrorNull);
 
+  // Connect to the websocket server
   useWebSocket(
     websocketURL,
     {
@@ -71,20 +86,12 @@ export default function Home() {
       },
       onClose: () => setError(Error("socket closed")),
       onError: () => setError(Error("socket error")),
+      // Reconnect with exponential backoff
       reconnectInterval: (attemptNumber) => Math.min(Math.pow(2, attemptNumber) * 1000, 10000),
+      // Reconnect indefinitely
       shouldReconnect: () => true,
     },
   );
-
-  //const setServers = (data) => {
-  //  setData(data);
-  //  return 5000;
-  //}
-  //const opts = {
-  //  revalidateOnFocus: false,
-  //  refreshInterval: setServers
-  //}
-  //const { error, isLoading } = useSWR('https://127.0.0.1:8080/list', getServers, opts);
 
   return (
     <div className="flex flex-row justify-center">
