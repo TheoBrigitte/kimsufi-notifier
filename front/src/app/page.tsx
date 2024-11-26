1dcc16fb4c6af4bb1043c821ce9a1c53d6afa12d
Theo Brigitte <theo.brigitte@gmail.com>
2024-11-26 22:18:19 +0100

    Add Telegram link

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -5,6 +5,9 @@ import ServersTable from './components/server';
 import useWebSocket  from 'react-use-websocket';
 import { Server } from './components/types';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTelegram } from '@fortawesome/free-brands-svg-icons';
+
 type ErrorNull = Error | null;
 
 type StatusProps = {
@@ -97,7 +100,13 @@ export default function Home() {
     <div className="flex flex-row justify-center">
       <div className="pt-10 pb-20 px-10">
         <div className="flex flex-row min-w-fit justify-center flex-nowrap text-nowrap">
-          <div className="basis-1/4 flex-none"></div>
+          <div className="basis-1/4 flex flex-col justify-center items-center">
+            <div>Receive notifications on Telegram</div>
+              <a className="flex text-blue-400 space-x-1" href="https://t.me/KimsufiNotifierBot">
+                <div><FontAwesomeIcon icon={faTelegram} /></div>
+                <div>t.me/KimsufiNotifierBot</div>
+              </a>
+          </div>
           <div className="basis-2/4 px-40 py-5 flex-none text-center text-xl font-bold">OVH Eco server availability</div>
           <Status
             error={error}
